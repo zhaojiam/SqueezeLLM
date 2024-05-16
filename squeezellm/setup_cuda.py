fc497ed4b5ab25818382c87ae8b546c07453d138
zhaojiam <zhaojia.ma@intel.com>
2024-05-16 17:23:32 +0800

    [sycl] Modify setup_cuda.py to compile but failed.

--- squeezellm/setup_cuda.py
+++ squeezellm/setup_cuda.py
@@ -5,7 +5,10 @@ setup(
     name="quant_cuda",
     ext_modules=[
         cpp_extension.CUDAExtension(
-            "quant_cuda", ["quant_cuda.cpp", "quant_cuda_kernel.cu"]
+            "quant_cuda", ["quant_cuda.cpp", "quant_cuda_kernel.dp.cpp"],
+            # extra_compile_args=['-fsycl', '-fsycl-targets=nvptx64-nvidia-cuda'],
+            extra_compile_args=['-fsycl'],
+            extra_link_flags=['']
         )
     ],
     cmdclass={"build_ext": cpp_extension.BuildExtension},
